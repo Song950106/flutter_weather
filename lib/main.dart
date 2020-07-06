bd02fd309f8f8728d47959a9514210e1fccf8436
song <song950106@outlook.com>
2020-07-06 23:12:56 +0800

    test flutter methodChannel

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
+final smallFront = TextStyle(color: Colors.white,fontSize:16);
+final double commonMargin = 60.0;
+final double smallMargin = 20.0;
+// index 2 should be variable
+List<String> paramsName = ["空气质量","PM2.5","东南风","体感温度","湿度","能见度","紫外线","气压"];
+List<String> paramsValue = ["良","28","7.2km/h","28℃","58%","19.9km","最弱","991hPa"];
+const platform = const MethodChannel('com.example.weather.weatherData');
+
 void main(){
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((value) => runApp(MyApp()));
 }
-
-  final smallFront = TextStyle(color: Colors.white,fontSize:16);
-  final double commonMargin = 60.0;
-  final double smallMargin = 20.0;
-// index 2 should be variable
-  List<String> paramsName = ["空气质量","PM2.5","东南风","体感温度","湿度","能见度","紫外线","气压"];
-  List<String> paramsValue = ["良","28","7.2km/h","28℃","58%","19.9km","最弱","991hPa"];
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -58,10 +58,22 @@ class _MainPageState extends State<MainPage>{
 
   _MainPageState(this._city,this._updateTime);
     final String _city ;
-    final String _updateTime;
+    String _updateTime;
+
+Future<void> _getWeatherJson(String city) async {
+  try{
+    String result  = await platform.invokeMethod('getWeatherData');
+    setState(() {
+      _updateTime  = result;
+    });
+  }on PlatformException catch (e){
+    print("get invoke method error. ${e.message}");
+  }
+}
 
   @override
   Widget build(BuildContext context) {
+    _getWeatherJson(_city);
     return Scaffold(
       appBar: WeatherAppBar(city: _city, updateTime: _updateTime),
       body: Center(
